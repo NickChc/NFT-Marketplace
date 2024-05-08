8152e3ec1765f60a21cc1d33a643dde66f92f688
NickChc <nika.chichua26@gmail.com>
2024-05-08 15:28:38 +0400

    feat: admin download route

---
+++ src/app/[lang]/admin/loading.tsx
@@ -0,0 +1,7 @@
+import { LoadingIcon } from "@/assets/icons";
+
+export default function AdminLoading() {
+  return (
+    <LoadingIcon className="animate-spin mx-auto mt-20 text-5xl font-extrabold" />
+  );
+}
