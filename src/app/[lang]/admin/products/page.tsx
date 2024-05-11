72b31d523b581280b2e6289dc1817e49200ec577
NickChc <nika.chichua26@gmail.com>
2024-05-11 16:46:23 +0400

    fix: redirect keep locale

--- src/app/[lang]/admin/products/page.tsx
+++ src/app/[lang]/admin/products/page.tsx
@@ -28,7 +28,7 @@ export default async function AdminProductsPage({
       <div className="flex flex-col sm:flex-row justify-between items-center gap-4">
         <PageHeader>{page.products}</PageHeader>
         <button className="rounded-md bg-purple-800 dark:bg-white dark:text-black text-white px-2 py-1 font-semibold whitespace-nowrap text-xs sm:text-sm">
-          <Link href={"/admin/products/new"}>{page.addProduct}</Link>
+          <Link href={`/${lang}/admin/products/new`}>{page.addProduct}</Link>
         </button>
       </div>
 
