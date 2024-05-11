0471427e28a5ba9f6de449166505772f3f723629
NickChc <nika.chichua26@gmail.com>
2024-05-11 16:45:46 +0400

    feat: query for products

--- src/app/[lang]/_api/getProducts.ts
+++ src/app/[lang]/_api/getProducts.ts
@@ -1,10 +1,22 @@
 import { TProduct } from "@/@types/general";
 import { productCollectionRef } from "@/firebase";
-import { getDocs } from "firebase/firestore";
+import { getDocs, orderBy, query, limit, where } from "firebase/firestore";
 
-export async function getProducts() {
+export async function getProducts(limitAt?: number, activeOnly?: boolean) {
   try {
-    const data = await getDocs(productCollectionRef);
+    let data;
+    if (limitAt && activeOnly) {
+      data = await getDocs(
+        query(
+          productCollectionRef,
+          where("isAvailable", "==", true),
+          orderBy("createdAt", "asc"),
+          limit(limitAt)
+        )
+      );
+    } else {
+      data = await getDocs(productCollectionRef);
+    }
     const products = data.docs.map((doc) => {
       const productData = doc.data();
       const createdAt = productData.createdAt.toDate();
