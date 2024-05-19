3674af21e7d3f846541aaa97ebb0f6dcd4a46bf6
NickChc <nika.chichua26@gmail.com>
2024-05-19 14:43:26 +0400

    register action

--- src/app/[lang]/(client)/auth/_components/RegisterForm/RegisterForm.tsx
+++ src/app/[lang]/(client)/auth/_components/RegisterForm/RegisterForm.tsx
@@ -12,7 +12,7 @@ export function RegisterForm() {
   const { page } = translations;
 
   return (
-    <form action={action} className="flex flex-col gap-y-4 mx-auto mt-9">
+    <form action={action} className="flex flex-col gap-y-4 mx-auto mt-9 p-3">
       <FormInput name="name" label={page.name} required />
       {error?.name && <div className="text-red-700">{error.name}</div>}
       <FormInput name="surname" label={page.surname} required />
