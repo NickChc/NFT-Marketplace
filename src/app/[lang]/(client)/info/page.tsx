7f79cef984cb0ab14258d7520fb03b987b04ce6c
NickChc <nika.chichua26@gmail.com>
2024-05-29 17:12:31 +0400

    feat: info page formed

--- src/app/[lang]/(client)/info/page.tsx
+++ src/app/[lang]/(client)/info/page.tsx
@@ -1,6 +1,8 @@
-import { getDictionaries } from "@/lib/dictionary";
 import { TLocale } from "../../../../../i18n.config";
-import { PageHeader } from "../../_components/PageHeader";
+import { getDictionaries } from "@/lib/dictionary";
+import { PageHeader } from "@/app/[lang]/_components/PageHeader";
+import { InfoPageCard } from "@/app/[lang]/(client)/info/_components/InfoPageCard";
+import WhatIsNFTImage from "@/assets/images/whatIsNFT.webp";
 
 interface InfoPageProps {
   params: {
@@ -12,8 +14,17 @@ export default async function InfoPage({ params: { lang } }: InfoPageProps) {
   const { page } = await getDictionaries(lang);
 
   return (
-    <>
-      <PageHeader>{page.info.toUpperCase()}</PageHeader>
-    </>
+    <div className="container xl:w-[90%] xl:mx-auto pb-9">
+      <PageHeader>{page.learnAboutNFT}</PageHeader>
+      <div className="mt-14 flex flex-col gap-6">
+        <InfoPageCard image={WhatIsNFTImage} title="What is NFT">
+          NFTs, or Non-Fungible Tokens, are unique digital assets verified using
+          blockchain technology. Unlike cryptocurrencies such as Bitcoin or
+          Ethereum, which are fungible and can be exchanged on a one-to-one
+          basis, each NFT has distinct information or attributes that make it
+          unique.
+        </InfoPageCard>
+      </div>
+    </div>
   );
 }
--- src/app/[lang]/_components/Header/Header.tsx
+++ src/app/[lang]/_components/Header/Header.tsx
@@ -35,7 +35,7 @@ export async function Header({
           </span>
         )}
       </Navigation>
-      <div className="flex justify-between sm:hidden px-4 py-1 sm:p-0 bg-purple-800 w-full items-center">
+      <div className="relative flex justify-between sm:hidden px-4 py-1 sm:p-0 bg-purple-800 w-full items-center">
         <div className="flex items-center gap-x-6">
           <LangSelect lang={lang} />
           <ToggleTheme />
