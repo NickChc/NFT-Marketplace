0ebd2f08e33167b4054ff9151455c6f0f056c0d4
unknown <nika.chichua26@gmail.com>
2024-07-23 18:58:26 +0400

    fix: deploy error: navigation (rename)

--- src/app/[lang]/_components/HeaderNav/HeaderNav.tsx
+++ src/app/[lang]/_components/HeaderNav/HeaderNav.tsx
@@ -1,6 +1,6 @@
 import { PropsWithChildren } from "react";
 
-export async function Navigation({ children }: PropsWithChildren) {
+export function HeaderNav({ children }: PropsWithChildren) {
   return (
     <nav className="w-full flex items-center justify-center sm:gap-x-6 lg:gap-x-9 bg-purple-800 relative overflow-hidden sm:overflow-visible">
       {children}
--- src/app/[lang]/_components/Navigation/index.ts
+++
@@ -1,1 +0,0 @@
-export { Navigation } from "@/app/[lang]/_components/Navigation/Navigation";
