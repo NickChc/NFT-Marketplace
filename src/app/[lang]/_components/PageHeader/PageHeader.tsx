6373307a2037dc4a3e1d384917f663356ff2b089
unknown <nika.chichua26@gmail.com>
2024-07-23 20:04:47 +0400

    style: logo size adjusted

--- src/app/[lang]/_components/PageHeader/PageHeader.tsx
+++ src/app/[lang]/_components/PageHeader/PageHeader.tsx
@@ -6,7 +6,7 @@ export function PageHeader({ children }: PropsWithChildren) {
     <div className="flex flex-col-reverse gap-y-4 sm:flex-row items-start sm:items-center justify-between mx-3 sm:mx-6 pt-4 ">
       <h1
         className="font-semibold text-xl sm:text-2xl
-      md:text-3xl lg:text-4xl 2xl:text-5xl"
+      md:text-3xl lg:text-4xl 2xl:text-4xl whitespace-nowrap"
       >
         {children}
       </h1>
