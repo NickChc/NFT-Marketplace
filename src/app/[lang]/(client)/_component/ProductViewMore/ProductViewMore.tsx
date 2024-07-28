bace7180521f2d84f9e6b6b0fa5eeef0139e4d55
unknown <nika.chichua26@gmail.com>
2024-07-28 18:23:40 +0400

    style: ProductViewMore overflow fixed

--- src/app/[lang]/(client)/_component/ProductViewMore/ProductViewMore.tsx
+++ src/app/[lang]/(client)/_component/ProductViewMore/ProductViewMore.tsx
@@ -70,26 +70,37 @@ export function ProductViewMore({ lang }: ProductViewMoreProps) {
 
   return (
     <div
-      className={`fixed min-h-dvh top-0 bottom-0 right-0 left-0 z-50 text-black place-items-center backdrop-blur-sm transition-display duration-300 start-style-b-t ${
-        mounted ? "grid" : "hidden"
+      className={`fixed min-h-dvh top-0 bottom-0 right-0 left-0 z-50 text-black flex-col items-center sm:place-items-center sm: py-6 backdrop-blur-custom transition-display duration-300 start-style-b-t ${
+        mounted ? "flex sm:grid" : "hidden"
       }`}
       onMouseDown={closeModal}
     >
+      {zoom && (
+        <div
+          className="fixed min-h-dvh top-0 bottom-0 right-0 left-0 z-50 bg-transparent backdrop-blur-custom"
+          onMouseDown={(e) => {
+            e.stopPropagation();
+            setZoom(false);
+          }}
+        ></div>
+      )}
       <div
-        className="w-[80%] bg-white py-6 relative border-solid border border-purple-700 rounded-md"
+        className={`w-[95%] xs:w-[90%] min-h-fit bg-white py-6 relative border-solid border border-purple-700 rounded-md ${
+          zoom ? "" : "overflow-y-visible overflow-x-hidden"
+        }`}
         onMouseDown={(e) => e.stopPropagation()}
       >
         <span
-          className="hover:opacity-75 text-xl text-purple-800 cursor-pointer absolute top-1 right-1 z-50"
+          className="hover:opacity-75 text-2xl text-purple-800 cursor-pointer absolute top-1 right-1 z-50"
           onClick={closeModal}
         >
           <CloseIcon />
         </span>
-        <div className="flex flex-col md:flex-row items-center md:items-stretch justify-center gap-x-3 p-3">
+        <div className="flex flex-col sm:flex-row items-center sm:items-stretch justify-center gap-x-3 pt-3 px-3">
           <div
-            className={`relative aspect-video pt-4 overflow-hidden w-full md:w-1/2 duration-200 z-50 ${
+            className={`relative aspect-video pt-4 w-full sm:w-1/2 duration-200 z-50 ${
               zoom
-                ? "cursor-zoom-out scale-125 md:scale-150 translate-y-10 md:translate-y-0 md:translate-x-1/2"
+                ? "cursor-zoom-out scale-125 sm:scale-150 translate-y-10 sm:translate-y-0 sm:translate-x-1/2"
                 : "cursor-zoom-in"
             } `}
             onClick={() => setZoom(!zoom)}
@@ -101,24 +112,26 @@ export function ProductViewMore({ lang }: ProductViewMoreProps) {
               objectFit="contain"
             />
           </div>
-          <div className="flex flex-col items-start justify-between w-full mt-3 md:mt-0 md:w-1/2 ">
+          <div className="flex flex-col items-start justify-between w-full mt-3 sm:mt-0 sm:w-1/2 ">
             <div className="flex flex-col min-h-40">
-              <h2 className="font-semibold mb-3 text-xl md:text-2xl">
+              <h2 className="font-semibold mb-3 text-xl sm:text-2xl">
                 {product.name}
               </h2>
-              <p className="max-h-28 overflow-auto pr-1 scrollbar-small ">
+              <p className="max-h-28 overflow-auto pr-1 scrollbar-small my-3">
                 {product.description}
               </p>
             </div>
-            <strong className="text-xl my-1">
-              {translations.page.price} -{" "}
-              {formatCurrency(product.priceInCents / 100)}
-            </strong>
-            <ProductCardButton
-              product={product}
-              lang={lang}
-              onClick={closeModal}
-            />
+            <div className="flex flex-col items-stretch w-full">
+              <strong className="text-xl mt-1 mb-4">
+                {translations.page.price} -{" "}
+                {formatCurrency(product.priceInCents / 100)}
+              </strong>
+              <ProductCardButton
+                product={product}
+                lang={lang}
+                onClick={closeModal}
+              />
+            </div>
           </div>
         </div>
       </div>
