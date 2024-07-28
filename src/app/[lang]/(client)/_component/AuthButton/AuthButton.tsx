d53f450ef9bcc6a87a7c04662583813dc1ff0672
unknown <nika.chichua26@gmail.com>
2024-07-28 19:01:25 +0400

    style: collection card hover

--- src/app/[lang]/(client)/_component/AuthButton/AuthButton.tsx
+++ src/app/[lang]/(client)/_component/AuthButton/AuthButton.tsx
@@ -28,7 +28,7 @@ export function AuthButton({ lang }: AuthButtonProps) {
   if (!mounted) {
     return (
       <button
-        className="rounded-full overflow-hidden  absolute top-1/2 -translate-y-1/2 right-20 bg-white text-purple-800 w-7 sm:w-8 md:w-10 aspect-square grid place-items-center opacity-75 cursor-default"
+        className="rounded-full overflow-hidden  absolute top-1/2 -translate-y-1/2 right-20 bg-white text-purple-800 w-8 md:w-10 aspect-square grid place-items-center opacity-75 cursor-default"
         disabled
       >
         <UserIcon />
@@ -37,9 +37,9 @@ export function AuthButton({ lang }: AuthButtonProps) {
   }
 
   return (
-    <button className="rounded-full sm:absolute top-1/2 sm:-translate-y-1/2 right-20 active:scale-90 transition-all duration-200">
+    <button className="rounded-full absolute top-1/2 -translate-y-1/2 right-3 sm:right-20 active:scale-90 transition-all duration-200">
       {loadingUser ? (
-        <span className="font-semibold bg-white w-7 sm:w-8 md:w-10 aspect-square grid place-items-center rounded-full">
+        <span className="font-semibold bg-white w-8 md:w-10 aspect-square grid place-items-center rounded-full">
           <LoadingIcon className="animate-spin text-xl text-purple-800" />
         </span>
       ) : currentUser ? (
@@ -47,14 +47,14 @@ export function AuthButton({ lang }: AuthButtonProps) {
           {notifications.length > 0 && (
             <Link
               href={`/${lang}/profile`}
-              className="bg-red-500 absolute -top-1 -right-1 text-white z-50 grid place-items-center rounded-full w-5 text-sm aspect-square"
+              className="bg-red-500 absolute -top-1 -right-1 text-white z-50 grid place-items-center rounded-full w-5 text-xs xs:text-sm aspect-square"
             >
               {notifications.length}
             </Link>
           )}
           <Link
             href={`/${lang}/profile`}
-            className="bg-white text-purple-800 w-7 sm:w-8 md:w-10 rounded-full aspect-square grid place-items-center"
+            className="bg-white text-purple-800 w-8 md:w-10 rounded-full aspect-square grid place-items-center"
           >
             {currentUser.name === ""
               ? currentUser.email[0].toLocaleUpperCase()
--- src/app/[lang]/(client)/profile/_components/Collection/CollectionCard/CollectionCard.tsx
+++ src/app/[lang]/(client)/profile/_components/Collection/CollectionCard/CollectionCard.tsx
@@ -41,12 +41,12 @@ export function CollectionCard({
             withoutImageParams !== "" ? `?${withoutImageParams}&` : "?"
           }image=${product.id}`}
         >
-          <div className="relative group aspect-video after:duration-300 after:content-[''] after:block after:w-0 hover:after:w-full after:h-[90%] after:border-solid after:border-2 after:border-white after:border-x-0 after:absolute after:top-1/2 after:right-1/2 after:-translate-y-1/2 after:translate-x-1/2 before:duration-300 before:content-[''] before:block before:h-0 hover:before:h-full before:w-[90%] before:border-2 before:border-solid before:border-white before:border-y-0 before:absolute before:top-1/2 before:right-1/2 before:translate-x-1/2 before:-translate-y-1/2 before:z-50 cursor-pointer">
+          <div className="relative aspect-video after:duration-300 after:content-[''] after:block after:w-0 [@media(hover:hover){&:hover}]:after:w-full after:h-[90%] after:border-solid after:border-2 after:border-purple-400 after:border-x-0 after:absolute after:top-1/2 after:right-1/2 after:-translate-y-1/2 after:translate-x-1/2 before:duration-300 before:content-[''] before:block before:h-0 [@media(hover:hover){&:hover}]:before:h-full before:w-[90%] before:border-2 before:border-solid before:border-purple-400 before:border-y-0 before:absolute before:top-1/2 before:right-1/2 before:translate-x-1/2 before:-translate-y-1/2 before:z-50 cursor-pointer active:brightness-75">
             <Image
               src={product.imagePath}
               alt={`${product.name} image`}
               fill
-              className="object-cover group-hover:brightness-75 duration-200"
+              className="object-cover duration-200"
             />
           </div>
         </Link>
