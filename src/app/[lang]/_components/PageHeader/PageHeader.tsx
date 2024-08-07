6f7dd1b2ee762c7665957f960f88d0809492b3af
unknown <nika.chichua26@gmail.com>
2024-08-07 14:29:24 +0400

    style: DashboardCard border

--- src/app/[lang]/_components/PageHeader/PageHeader.tsx
+++ src/app/[lang]/_components/PageHeader/PageHeader.tsx
@@ -6,7 +6,7 @@ export function PageHeader({ children }: PropsWithChildren) {
     <div className="flex flex-col-reverse gap-y-4 xs:flex-row items-start sm:items-center justify-between mx-3 sm:mx-6 pt-4 overflow-hidden">
       <article
         className="font-semibold text-base sm:text-2xl
-      md:text-3xl lg:text-4xl 2xl:text-4xl whitespace-nowrap"
+      md:text-3xl lg:text-4xl 2xl:text-4xl whitespace-nowrap py-1"
       >
         {children}
       </article>
--- src/app/[lang]/admin/_components/DashboardCard/DashboardCard.tsx
+++ src/app/[lang]/admin/_components/DashboardCard/DashboardCard.tsx
@@ -6,8 +6,10 @@ interface DashboardCardProps {
 
 export function DashboardCard({ title, subtitle, body }: DashboardCardProps) {
   return (
-    <div className="p-3 rounded-3 flex flex-col min-w-full mx-4 overflow-hidden">
-      <h2 className="font-semibold text-xl sm:text-2xl md:text-3xl whitespace-nowrap mb-4">{title}</h2>
+    <div className="p-3 rounded-3 flex flex-col min-w-full mx-4 overflow-hidden shadow-lg rounded-lg border-solid border-2 border-gray-900 dark:border-gray-500">
+      <h2 className="font-semibold text-xl sm:text-2xl md:text-3xl whitespace-nowrap mb-4">
+        {title}
+      </h2>
       <h3 className="py-3 whitespace-nowrap lg:text-xl">{subtitle}</h3>
       <h4 className="text-gray-500">{body}</h4>
     </div>
